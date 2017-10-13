cbaa1b71d3cea98d84e1811cbfbc98d930eef4e8
Chris Underwood <cunderwoodmn@gmail.com>
2017-10-12 18:22:37 -0700

    put in the js, html, & css to create layout for the base page

--- app.js
+++ app.js
@@ -13,9 +13,27 @@ console.log("Connected to App.js");
 
   function randomUserData(data, textStatus, jqXHR) {
     console.log("inside randomUserData ", data);
-    console.log("inside randomUserData ", textStatus);
-    console.log("inside randomUserData ", jqXHR);
-    console.log(data.results[4].name.last);
+    //console.log("inside randomUserData ", textStatus);
+    //console.log("inside randomUserData ", jqXHR);
+    //console.log(data.results[4].name.last);
+    var addHtml = '';
+      data.results.forEach(function (item, index, arr) {
+        console.log("each thumbnail " + data.results[index].picture.large);
+        console.log("each first name " + data.results[index].name.first);
+        console.log("each last name " + data.results[index].name.last);
+        console.log("each email " + data.results[index].email);
+        console.log("each city " + data.results[index].location.city);
+        addHtml += '<div class="col-md-3 row person ">';
+
+        addHtml += '<div><img class="thumbnail" src="' + data.results[index].picture.thumbnail + '"></div>';
+        addHtml += '<div>';
+        addHtml += '<span class="firstName">' + data.results[index].name.first + ' ';
+        addHtml +=  data.results[index].name.last + '</span>';
+        addHtml += '<div class="email">' + data.results[index].email + '</div>';
+        addHtml += '<div class="city">' + data.results[index].location.city + '</div></div>';
+        addHtml += '</div>';
+      });
+        document.getElementById('cards').innerHTML = addHtml;
   }
 
   $.ajax({
